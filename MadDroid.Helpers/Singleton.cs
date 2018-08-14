0b61bbb53512d84982a0046831acc21791fa9417
Mateus Becker <mateusbecker17@outlook.com>
2018-08-14 07:47:50 -0300

    Add Singleton Helper

---
+++ MadDroid.Helpers/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace MadDroid.Helpers
+{
+    /// <summary>
+    /// A Helper class to create singletons
+    /// </summary>
+    /// <typeparam name="T">The type of the object to get the singleton instace</typeparam>
+    public static class Singleton<T> where T : new()
+    {
+        private static ConcurrentDictionary<Type, T> _instances = new ConcurrentDictionary<Type, T>();
+
+        /// <summary>
+        /// Gets the instace of the object
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                // Gets or add the instance to the ConcurrentDictionary
+                return _instances.GetOrAdd(typeof(T), (t) => new T());
+            }
+        }
+    }
+}
