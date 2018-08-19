4a40f03f439d3c84fbf39384a8d3305747818c50
Mateus Becker <mateusbecker17@outlook.com>
2018-08-19 17:47:01 -0300

    Added a web helper to get strings

---
+++ MadDroid.Helpers/Web.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MadDroid.Helpers
+{
+    /// <summary>
+    /// A helper class to get stuff from web
+    /// </summary>
+    public static class Web
+    {
+        /// <summary>
+        /// Get a string async
+        /// </summary>
+        /// <param name="url">The url to get the string</param>
+        /// <returns></returns>
+        public static async Task<string> GetStringAsync(string url)
+        {
+            // Create the client
+            using (var client = new HttpClient())
+            {
+                // Get and return the string
+                return await client.GetStringAsync(url);
+            }
+        }
+    }
+}
