17dc2561efc2bb788d911ef99354ac147a293d90
Mateus Becker <mateusbecker17@outlook.com>
2019-01-01 19:39:03 -0200

    Added string extension IsNullOrEmpty

---
+++ MadDroid.Helpers/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadDroid.Helpers
+{
+    /// <summary>
+    /// Class extensions for <see cref="string"/>
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Indicates whether the specified string is null or an <see cref="string.Empty"/> string.
+        /// </summary>
+        /// <param name="value">The string to test.</param>
+        /// <returns>true if the value parameter is null or an empty string (""); otherwise, false.</returns>
+        public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+    }
+}
